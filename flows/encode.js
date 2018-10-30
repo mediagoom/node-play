35f756a4ddfb520b8bc2392ecbd23d912e37f7d6
aseduto <aseduto@hotmail.com>
2018-10-30 17:53:23 +0100

    WIP. ffmpeg flow configuration

--- flows/encode.js
+++ flows/encode.js
@@ -1,8 +1,18 @@
+const ffprobe_output = require('./ffprobe_output');
 
 const duration_rx = 'Duration: (\\d\\d):(\\d\\d):(\\d\\d).(\\d\\d), start: ([\\d\\.]+), bitrate: (\\d+) kb/s';
 const stream_rx = 'Stream\\s#0:(\\d+)(?:[\\(\\[](\\w+)[\\)\\]]){0,1}:\\s(Audio|Video):.*?(?:(?:,\\s(\\d+)x(\\d+))|(?:(\\d+) Hz)).*?, (?:(?:(\\d+) kb/s)|(?:stereo)|(?:.*? fps))';
-           
+
+const begin_config_code = `
+    const file = propertyBag.input_file = config.input_file;
+    //let configure next operation file probe
+    propertyBag.config.args = [ file ];
+
+    file
+`;
+
 const probe_code = `
+
           let output = propertyBag.parent.result;
 
           let regexpd = new RegExp(config.duration_rx, 'g');
@@ -39,9 +49,75 @@ const probe_code = `
 
         }
     
-    streams.splice(-1, 1);
+    //streams.splice(-1, 1);
 
     JSON.stringify(streams, null, 4);
+
+`;
+
+const encode_config_code = `
+
+        const streams = JSON.parse(propertyBag.parent.result);
+
+        cmd_video = '-vf "scale=w=$(width):h=$(height)" -codec:v libx264 -profile:v high -level 31 -b:v $(vb)k -r 25 -g 50 -sc_threshold 0 -x264opts ratetol=0.1 -minrate $(vb)k -maxrate $(vb)k -bufsize $(vb)k'
+        cmd_audio = '-b:a $(ab)k -codec:a aac -profile:a aac_low -ar 44100 -ac 2 -y'
+
+        cmd_encode = '-i "$(file)" $(map)$(video)$(audio) "$(outputfile)"'
+        const file = propertyBag.input_file;
+       
+        cmd_encode = cmd_encode.replace('$(file)', file);
+
+        let audio = null;
+        let video = null;
+
+        for(let idx = 0; idx < streams.length; idx++)
+        {
+            const stream = streams[idx];
+
+            if('Video' === stream.kind && null === video)
+                video = stream;
+            
+            if('Audio' === stream.kind && null === audio)
+                audio = stream;
+        }
+
+        let map = ''
+
+        if(null !== audio)
+        {
+            map += '-map 0:' + audio.index;
+            map += ' '
+
+        }
+        else
+        {
+            cmd_audio = '';
+        }
+
+        if(null !== video)
+        {
+            map += '-map 0:' + video.index;
+            map += ' '
+        }
+        else
+        {
+            cmd_video = '';
+        }
+
+        cmd_encode = cmd_encode.replace('$(map)', map);
+        cmd_encode = cmd_encode.replace('$(video)', cmd_video);
+        cmd_encode = cmd_encode.replace('$(audio)', cmd_audio);
+        
+        cmd_encode = cmd_encode.replace('$(width)', config.width);
+        cmd_encode = cmd_encode.replace('$(height)', config.height);
+
+        
+
+        let j = cmd_encode.split(' ');
+
+        propertyBag.info = JSON.stringify(j);
+
+        cmd_encode
 
 `;
 
@@ -53,16 +129,45 @@ module.exports = {
     {
         root : {
             type : 'START', name : 'START', children : [
-                { type : 'code', name : 'run ffprobe'
+                { type : 'code', name : 'pre configuration'
                     , config : {
-                        cmd : 'ffprobe'
-                        , args : 'TODO: file-path'
-                        , code : ' "this is the ffprobe return" '
+                        input_file : '/tmp/media/file.mp4'
+                        , code : begin_config_code
                     }
                     , children : [
-                        {type : 'code', name : 'process streams and config', config : { code : ' "do code processing pre encoding" '}
+                        { type : 'code', name : 'run ffprobe'
+                            , config : {
+                                cmd : 'ffprobe'
+                                , args : ['TODO: file-path']
+                                , code : ` 
+                        
+                        propertyBag.ff_arg = '[' + config.args[0] + ']';
+                                
+                        \`
+                           
+                            
+
+                            ${ffprobe_output}
+                        \`
+                        `
+                            }
                             , children : [
-                                {type : 'END', name : 'PACKAGE'}
+                                {type : 'code', name : 'process streams and config'
+                                    , config : { code : probe_code
+                                        , duration_rx
+                                        , stream_rx 
+                                    }
+                                    , children : [
+                                        {type : 'code', name : 'preprocess 1'
+                                            , config : { code : encode_config_code
+                                              
+                                            }
+                                            , children : [
+                                                {type : 'END', name : 'PACKAGE'}
+                                            ]
+                                        } 
+                                    ]
+                                }
                             ]
                         }
                     ]
